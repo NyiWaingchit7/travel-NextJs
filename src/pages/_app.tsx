d6e1b4e8d0c35f43b2b918e9e8fd208df93e3e28
KyawZin-Thet <128166677+KyawZin-Thet@users.noreply.github.com>
2024-01-11 10:22:12 +0630

    added nextAuth and middleware

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,20 @@ import { store } from "@/store";
 import "@/styles/globals.css";
 import { theme } from "@/utils/theme";
 import { ThemeProvider } from "@emotion/react";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </Provider>
+    <SessionProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </Provider>
+    </SessionProvider>
   );
 }
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,10 @@
 interface ConfigType {
   apiBaseUrl: string;
+  googleClientId: string;
+  googleClientSecret: string;
 }
 export const config: ConfigType = {
   apiBaseUrl: process.env.NEXR_PUBLIC_API_BASE_URL || "",
+  googleClientId: process.env.GOOGLE_CLIENT_ID || "",
+  googleClientSecret: process.env.GOOFLE_CLIENT_SECRET || "",
 };
